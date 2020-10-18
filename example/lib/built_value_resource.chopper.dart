5f2e117e1d64e6ba85b83e3b22d7ab8562e16030
Hadrien Lejard <hadrien.lejard@gmail.com>
2020-10-18 16:31:25 +0200

    Re-add Angular example and fix travis (#181)
    Re add example removed here (https://github.com/lejard-h/chopper/pull/178/files)
    
    Upgrade to Angular 6, but I had to remove jaguar_serializer which is not maintain anymore (last update 1 year ago).
    
    Then use new Dart command to run pub, analyzer and formatter

--- example/lib/built_value_resource.chopper.dart
+++ example/lib/built_value_resource.chopper.dart
@@ -6,26 +6,31 @@ part of resource;
 // ChopperGenerator
 // **************************************************************************
 
+// ignore_for_file: always_put_control_body_on_new_line, always_specify_types, prefer_const_declarations
 class _$MyService extends MyService {
   _$MyService([ChopperClient client]) {
     if (client == null) return;
     this.client = client;
   }
 
+  @override
   final definitionType = MyService;
 
-  Future<Response> getResource(String id) {
-    final $url = '/resources/${id}/';
+  @override
+  Future<Response<dynamic>> getResource(String id) {
+    final $url = '/resources/$id/';
     final $request = Request('GET', $url, client.baseUrl);
     return client.send<dynamic, dynamic>($request);
   }
 
+  @override
   Future<Response<BuiltList<Resource>>> getBuiltListResources() {
     final $url = '/resources/list';
     final $request = Request('GET', $url, client.baseUrl);
     return client.send<BuiltList<Resource>, Resource>($request);
   }
 
+  @override
   Future<Response<Resource>> getTypedResource() {
     final $url = '/resources/';
     final $headers = {'foo': 'bar'};
@@ -33,6 +38,7 @@ class _$MyService extends MyService {
     return client.send<Resource, Resource>($request);
   }
 
+  @override
   Future<Response<Resource>> newResource(Resource resource, {String name}) {
     final $url = '/resources';
     final $headers = {'name': name};
