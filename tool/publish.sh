c081324cdd3e7db64624464170abbd78f3fba61f
Hadrien Lejard <hadrien@lefty.io>
2021-05-17 16:11:19 +0200

    publish

--- tool/publish.sh
+++ tool/publish.sh
@@ -4,7 +4,7 @@ PKG=$1
 echo -e "\033[1mPKG: ${PKG}\033[22m"
 pushd "${PKG}"
 
-mkdir -p .pub-cache
+mkdir -p ~/.pub-cache
 
 echo $CREDENTIAL_JSON > ~/.pub-cache/credentials.json
 
@@ -12,5 +12,5 @@ sed '/Comment before publish$/,+2 d' pubspec.yaml > pubspec.temp.yaml
 rm pubspec.yaml
 mv pubspec.temp.yaml pubspec.yaml
 
-dart pub publish --dry-run
+dart pub publish
 popd
