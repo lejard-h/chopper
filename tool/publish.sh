b5df905b8b8b889b55f531135b678af4397dd670
Hadrien Lejard <hadrien@lefty.io>
2021-05-17 15:32:15 +0200

    github actions publish workflow

--- tool/publish.sh
+++ tool/publish.sh
@@ -1,23 +1,12 @@
 #!/usr/bin/env bash
 
-if [ -z "$PKG" ]; then
-  echo -e '\033[31mPKG environment variable must be set!\033[0m'
-  exit 1
-fi
-
+PKG=$1
 echo -e "\033[1mPKG: ${PKG}\033[22m"
 pushd "${PKG}"
 
 mkdir -p .pub-cache
 
-cat <<EOF > ~/.pub-cache/credentials.json
-{
-  "accessToken":"$accessToken",
-  "refreshToken":"$refreshToken",
-  "tokenEndpoint":"$tokenEndpoint",
-  "scopes": ["https://www.googleapis.com/auth/userinfo.email","openid"],
-  "expiration":$expiration
-}
-EOF
+echo $CREDENTIAL_JSON > ~/.pub-cache/credentials.json
 
-dart pub publish -f
+dart pub publish -f --dry-run
+popd
