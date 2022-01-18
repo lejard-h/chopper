cac6079d28ca8d12795fb62d8a282947e1649338
Max Röhrl <max.roehrl11@gmail.com>
2022-01-18 14:28:40 +0100

    Add the original request as an additional argument to authenticate()

--- chopper/lib/src/authenticator.dart
+++ chopper/lib/src/authenticator.dart
@@ -5,5 +5,5 @@ import 'package:chopper/chopper.dart';
 /// This method should return a [Request] that includes credentials to satisfy an authentication challenge received in
 /// [response]. It should return `null` if the challenge cannot be satisfied.
 abstract class Authenticator {
-  FutureOr<Request?> authenticate(Request request, Response response);
+  FutureOr<Request?> authenticate(Request request, Response response, Request originalRequest);
 }
