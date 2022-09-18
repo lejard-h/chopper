d1fa1ee86d7092353e0f6855a96bc2f3bf5c2867
Klemen Tušar <techouse@gmail.com>
2022-09-18 13:55:36 +0100

    :technologist: Add an example using Squadron and a WorkerPool

---
+++ example/lib/json_decode_service.activator.g.dart
@@ -0,0 +1,9 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// **************************************************************************
+// SquadronWorkerGenerator
+// **************************************************************************
+
+import 'json_decode_service.vm.g.dart';
+
+final $JsonDecodeServiceActivator = $getJsonDecodeServiceActivator();
