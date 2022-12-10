80cc8088ea38c6f2fa92e3f095b989e86cc7aa7a
Klemen Tušar <techouse@gmail.com>
2022-12-10 14:03:00 +0000

    :white_check_mark: Add authenticator tests

--- chopper/test/authenticator_test.dart
+++ chopper/test/authenticator_test.dart
@@ -1,4 +1,4 @@
-import 'dart:convert';
+import 'dart:convert' show jsonEncode;
 
 import 'package:chopper/chopper.dart';
 import 'package:http/http.dart' as http;
@@ -180,6 +180,198 @@ void main() async {
       final response = await chopper.post(
         Uri(
           path: '/test/post',
+          queryParameters: {'key': 'val'},
+        ),
+        headers: {'int': '42'},
+        body: {
+          'name': 'john',
+          'surname': 'doe',
+        },
+      );
+
+      expect(response.body, equals('ok'));
+      expect(response.statusCode, equals(200));
+      expect(tested['authenticated'], equals(true));
+      expect(tested['unauthenticated'], equals(true));
+
+      httpClient.close();
+    });
+  });
+
+  group('PUT', () {
+    test('authorized', () async {
+      final httpClient = MockClient((request) async {
+        expect(
+          request.url.toString(),
+          equals('$baseUrl/test/put?key=val'),
+        );
+        expect(request.method, equals('PUT'));
+        expect(request.headers['foo'], equals('bar'));
+        expect(request.headers['int'], equals('42'));
+        expect(
+          request.body,
+          jsonEncode(
+            {
+              'name': 'john',
+              'surname': 'doe',
+            },
+          ),
+        );
+
+        return http.Response('ok', 200);
+      });
+
+      final chopper = buildClient(httpClient);
+      final response = await chopper.put(
+        Uri(
+          path: '/test/put',
+          queryParameters: {'key': 'val'},
+        ),
+        headers: {'int': '42'},
+        body: {
+          'name': 'john',
+          'surname': 'doe',
+        },
+      );
+
+      expect(response.body, equals('ok'));
+      expect(response.statusCode, equals(200));
+
+      httpClient.close();
+    });
+
+    test('unauthorized', () async {
+      final httpClient = MockClient((request) async {
+        expect(
+          request.url.toString(),
+          equals('$baseUrl/test/put?key=val'),
+        );
+        expect(request.method, equals('PUT'));
+        expect(request.headers['foo'], equals('bar'));
+        expect(request.headers['int'], equals('42'));
+        expect(
+          request.body,
+          jsonEncode(
+            {
+              'name': 'john',
+              'surname': 'doe',
+            },
+          ),
+        );
+
+        if (!authenticated) {
+          tested['unauthenticated'] = true;
+          authenticated = true;
+
+          return http.Response('unauthorized', 401);
+        } else {
+          tested['authenticated'] = true;
+          expect(request.headers['authorization'], equals('some_fake_token'));
+        }
+
+        return http.Response('ok', 200);
+      });
+
+      final chopper = buildClient(httpClient);
+      final response = await chopper.put(
+        Uri(
+          path: '/test/put',
+          queryParameters: {'key': 'val'},
+        ),
+        headers: {'int': '42'},
+        body: {
+          'name': 'john',
+          'surname': 'doe',
+        },
+      );
+
+      expect(response.body, equals('ok'));
+      expect(response.statusCode, equals(200));
+      expect(tested['authenticated'], equals(true));
+      expect(tested['unauthenticated'], equals(true));
+
+      httpClient.close();
+    });
+  });
+
+  group('PATCH', () {
+    test('authorized', () async {
+      final httpClient = MockClient((request) async {
+        expect(
+          request.url.toString(),
+          equals('$baseUrl/test/patch?key=val'),
+        );
+        expect(request.method, equals('PATCH'));
+        expect(request.headers['foo'], equals('bar'));
+        expect(request.headers['int'], equals('42'));
+        expect(
+          request.body,
+          jsonEncode(
+            {
+              'name': 'john',
+              'surname': 'doe',
+            },
+          ),
+        );
+
+        return http.Response('ok', 200);
+      });
+
+      final chopper = buildClient(httpClient);
+      final response = await chopper.patch(
+        Uri(
+          path: '/test/patch',
+          queryParameters: {'key': 'val'},
+        ),
+        headers: {'int': '42'},
+        body: {
+          'name': 'john',
+          'surname': 'doe',
+        },
+      );
+
+      expect(response.body, equals('ok'));
+      expect(response.statusCode, equals(200));
+
+      httpClient.close();
+    });
+
+    test('unauthorized', () async {
+      final httpClient = MockClient((request) async {
+        expect(
+          request.url.toString(),
+          equals('$baseUrl/test/patch?key=val'),
+        );
+        expect(request.method, equals('PATCH'));
+        expect(request.headers['foo'], equals('bar'));
+        expect(request.headers['int'], equals('42'));
+        expect(
+          request.body,
+          jsonEncode(
+            {
+              'name': 'john',
+              'surname': 'doe',
+            },
+          ),
+        );
+
+        if (!authenticated) {
+          tested['unauthenticated'] = true;
+          authenticated = true;
+
+          return http.Response('unauthorized', 401);
+        } else {
+          tested['authenticated'] = true;
+          expect(request.headers['authorization'], equals('some_fake_token'));
+        }
+
+        return http.Response('ok', 200);
+      });
+
+      final chopper = buildClient(httpClient);
+      final response = await chopper.patch(
+        Uri(
+          path: '/test/patch',
           queryParameters: {'key': 'val'},
         ),
         headers: {'int': '42'},
