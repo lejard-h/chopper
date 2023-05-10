a50468c990bbcff960d2c71333a9e63cf668b98e
Klemen Tušar <techouse@gmail.com>
2023-05-10 22:55:01 +0100

    :arrow_up: Bump Dart constraint to ">=2.17.0 <3.0.0"

--- example/lib/built_value_resource.chopper.dart
+++ example/lib/built_value_resource.chopper.dart
@@ -1,6 +1,6 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
 
-part of resource;
+part of 'built_value_resource.dart';
 
 // **************************************************************************
 // ChopperGenerator
