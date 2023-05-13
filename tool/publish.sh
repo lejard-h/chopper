5b2fd04d47e851e45b90db8f181ff60abf030458
Klemen Tušar <techouse@gmail.com>
2023-05-13 17:44:03 +0100

    :green_heart: Fix CI publish

--- tool/publish.sh
+++ tool/publish.sh
@@ -6,10 +6,6 @@ PKG=$1
 echo -e "\033[1mPKG: ${PKG}\033[22m"
 pushd "${PKG}"
 
-mkdir -p ~/.pub-cache
-
-echo $CREDENTIAL_JSON > ~/.pub-cache/credentials.json
-
 sed '/Comment before publish$/,+2 d' pubspec.yaml > pubspec.temp.yaml
 rm pubspec.yaml
 mv pubspec.temp.yaml pubspec.yaml
