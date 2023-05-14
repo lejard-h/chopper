d2d8d2c9c591098d1258705d363e6b04ed911d31
Klemen Tušar <techouse@gmail.com>
2023-05-14 14:34:35 +0100

    :green_heart: Fix CI coverage reporting

--- tool/coverage.sh
+++ tool/coverage.sh
@@ -1,24 +1,7 @@
 #!/bin/bash
-
-# ensure codecov token exists
-if [ -z "$CODECOV_TOKEN" ]; then
-  echo "CODECOV_TOKEN is not set"
-  exit 1
-fi
-
 # install coverage tools
 dart pub global activate coverage
 dart pub global activate remove_from_coverage
-
-# install codecov uploader
-curl -Os https://uploader.codecov.io/latest/linux/codecov
-chmod +x codecov
-
-# ensure codecov uploaded downloaded
-if [ ! -f codecov ]; then
-  echo "codecov uploader not found"
-  exit 1
-fi
 
 ## loop through all the packages
 PKGS=("chopper" "chopper_generator" "chopper_build_value")
@@ -26,7 +9,7 @@ PKGS=("chopper" "chopper_generator" "chopper_build_value")
 for PKG in "${PKGS[@]}"
 do
   # move to package directory
-  cd "$PKG" || exit
+  pushd "$PKG" || exit
   # install dependencies
   dart pub get
   # run tests with coverage
@@ -34,7 +17,7 @@ do
   # remove generated files from coverage
   dart pub global run remove_from_coverage:remove_from_coverage -f coverage/lcov.info -r '\.g\.dart$'
   # if coverage/lcov.info exists, upload to codecov
-  [ -f coverage/lcov.info ] && ../codecov -t "$CODECOV_TOKEN" -f coverage/lcov.info
+  curl -s https://codecov.io/bash | bash
   # move back to root directory
-  cd - || exit
+  popd - || exit
 done
