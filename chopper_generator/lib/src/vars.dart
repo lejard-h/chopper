97959f0773d40e0e3ce21fd87a6e025e283012ad
Klemen Tusar <techouse@gmail.com>
2023-07-04 22:01:47 +0100

    :recycle: refactor chopper_generator (#448)

---
+++ chopper_generator/lib/src/vars.dart
@@ -0,0 +1,17 @@
+enum Vars {
+  client('client'),
+  baseUrl('baseUrl'),
+  parameters(r'$params'),
+  headers(r'$headers'),
+  request(r'$request'),
+  body(r'$body'),
+  parts(r'$parts'),
+  url(r'$url');
+
+  const Vars(this.name);
+
+  final String name;
+
+  @override
+  String toString() => name;
+}
