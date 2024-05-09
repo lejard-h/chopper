df9801ada11d6a95159b43f250d3a2f630a1dfcc
Klemen Tusar <techouse@gmail.com>
2024-05-09 08:59:09 +0100

    :bookmark: release v8.0.0 (#609)
    # chopper
    
    ## 8.0.0
    
    - Restructure interceptors ([#547](https://github.com/lejard-h/chopper/pull/547))
    - Add per-request timeout ([#604](https://github.com/lejard-h/chopper/pull/604))
    
    # chopper_generator
    
    ## 8.0.0
    
    - Restructure interceptors ([#547](https://github.com/lejard-h/chopper/pull/547))
    - Add per-request timeout ([#604](https://github.com/lejard-h/chopper/pull/604))
    
    ## 3.0.0
    
    - Require Chopper ^8.0.0
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: Job Guldemeester <job@dutchcodingcompany.com>

--- example/lib/json_decode_service.activator.g.dart
+++ example/lib/json_decode_service.activator.g.dart
@@ -1,7 +1,7 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
 
 // **************************************************************************
-// Generator: WorkerGenerator 2.4.1
+// Generator: WorkerGenerator 2.4.2
 // **************************************************************************
 
 import 'json_decode_service.vm.g.dart';
