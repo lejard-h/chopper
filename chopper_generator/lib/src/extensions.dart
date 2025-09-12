a3b66b2aa4532f26f4dbc60471ce85684b49014a
Klemen Tusar <techouse@gmail.com>
2025-09-12 20:06:58 +0100

    :bulb: add documentation to DartTypeExtension

--- chopper_generator/lib/src/extensions.dart
+++ chopper_generator/lib/src/extensions.dart
@@ -2,5 +2,6 @@ import 'package:analyzer/dart/element/nullability_suffix.dart';
 import 'package:analyzer/dart/element/type.dart';
 
 extension DartTypeExtension on DartType {
+  /// Returns true if the type is nullable.
   bool get isNullable => nullabilitySuffix != NullabilitySuffix.none;
 }
